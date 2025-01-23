b77c4b8626886d7b52d464333bb611a2bb31d87e
Angelina Faggiano <adeluca43@gmail.com>
2025-01-23 09:51:12 -0600

    added harvester.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,9 @@
 import { createPlan} from "./plan.js"
 import { createAsparagus } from "./seeds/asparagus.js"
 import { createCorn } from "./seeds/corn.js"
-
+import { harvestPlants } from "./harvester.js"
 
 console.log("Welcome to the main module")
 const yearlyPlan = createPlan()
-
 
 
